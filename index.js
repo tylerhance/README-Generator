3c1a23226c22913b3b38059a2b2648b01009e185
Tyler Hance <tyler.hance@gmail.com>
2021-05-26 20:56:16 -0700

    Adding functions and logic for questions to generate readme in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-
+const generateReadme = require("./generateReadme");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const promptUser = () => {
@@ -13,24 +13,72 @@ const promptUser = () => {
         },
         {
            type: 'input',
-           name: 'title',
-           message: '', 
+           name: 'description',
+           message: 'Write a description of your project: ', 
         },
         {
            type: 'input',
-           name: 'title',
-           message: '', 
+           name: 'installation',
+           message: 'Briefly describe the installation process: ', 
         },
         {
            type: 'input',
-           name: 'title',
-           message: '', 
+           name: 'usage',
+           message: 'What is this project usage?', 
+        },
+        {
+           type: 'list',
+           name: 'license',
+           message: 'Choose the license for the project: ',
+           choices: [
+              "Apache",
+              "Academic",
+              "GNU",
+              "ISC",
+              "MIT",
+              "Mozilla",
+              "Open",
+           ] 
         },
         {
            type: 'input',
-           name: 'title',
-           message: '', 
+           name: 'contributing',
+           message: 'Who are the contributors of this project?', 
+        },
+        {
+           type: 'input',
+           name: 'tests',
+           message: 'Is there a test included?', 
+        },
+        {
+           type: 'input',
+           name: 'questions',
+           message: 'What do I do if I have an issue? ', 
+        },
+        {
+           type: 'input',
+           name: 'username',
+           message: 'Please enter your GitHub username: ', 
+        },
+        {
+           type: 'input',
+           name: 'email',
+           message: 'Please enter your email: ', 
         },
        
     ])
 }
+
+   async function init() {
+      try {
+         const answers = await promptUser();
+         const generateContent = generateReadme(answers);
+         
+         await writeFileAsync('./dist/README.md', generateContent);
+         console.log('Success, wrote to README.md');
+      } catch(err) {
+         console.log(err);
+      }
+   }
+
+   init();
